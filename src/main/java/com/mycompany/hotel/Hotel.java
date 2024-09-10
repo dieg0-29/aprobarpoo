d05d8a710935bb9452f90e938032a02329e568c7
Jostin <Jostin@DESKTOP-2MILMF1>
2024-09-09 21:33:35 -0500

    cambie de texto

--- src/main/java/com/mycompany/hotel/Hotel.java
+++ src/main/java/com/mycompany/hotel/Hotel.java
@@ -11,6 +11,6 @@ package com.mycompany.hotel;
 public class Hotel {
 
     public static void main(String[] args) {
-        System.out.println("Sotelo es tremendo ponderador");
+        System.out.println("la bica da poder");
     }
 }
