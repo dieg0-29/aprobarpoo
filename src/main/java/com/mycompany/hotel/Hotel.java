ad2b379ac14536f4719c4a8e5088bd579f6b343f
novacalderon <jostindcalderon@gmail.com>
2024-09-09 21:30:33 -0500

    Update Hotel.java

--- src/main/java/com/mycompany/hotel/Hotel.java
+++ src/main/java/com/mycompany/hotel/Hotel.java
@@ -11,6 +11,6 @@ package com.mycompany.hotel;
 public class Hotel {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("La bica da poder");
     }
 }
