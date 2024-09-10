bed965d31ea15097dbd94d5b4661f5b53c20eaef
Jostin <Jostin@DESKTOP-2MILMF1>
2024-09-09 22:14:19 -0500

    .

--- src/main/java/com/mycompany/hotel/Hotel.java
+++ src/main/java/com/mycompany/hotel/Hotel.java
@@ -1,16 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.hotel;
-
-/**
- *
- * @author Jostin
- */
 public class Hotel {
-
     public static void main(String[] args) {
-        System.out.println("cada vez los kchimbos son mas hambrientos");
+        System.out.println("23-2>24-1");
     }
 }
