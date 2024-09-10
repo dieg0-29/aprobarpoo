69c061beceb20df4a5cc23d2451c8f0150735570
novacalderon <jostindcalderon@gmail.com>
2024-09-09 22:17:55 -0500

    actualice desde github

--- src/main/java/com/mycompany/hotel/Hotel.java
+++ src/main/java/com/mycompany/hotel/Hotel.java
@@ -2,5 +2,6 @@ package com.mycompany.hotel;
 public class Hotel {
     public static void main(String[] args) {
         System.out.println("al delegado lo aprobaron y lo probaron");
+        System.out.println("Esto servira?");
     }
 }
